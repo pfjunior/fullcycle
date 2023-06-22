4e52888455a5d4665302160cb8a8adbe8cda92b4
pfjunior <pf.junior89@hotmail.com>
2023-06-22 08:25:57 -0300

    entidades - aula 6

--- ddd-modelagem-patterns/src/entities/customer.ts
+++ ddd-modelagem-patterns/src/entities/customer.ts
@@ -2,16 +2,34 @@ class Customer {
     _id: string;
     _name: string;
     _addess: string;
-    _active: boolean = true;
+    _active: boolean = false;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._addess = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+    }
+
+    validateAddress(): boolean {
+        if (this._addess.length === 0) {
+            return true;
+        }
+        return false;
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     activate() {
