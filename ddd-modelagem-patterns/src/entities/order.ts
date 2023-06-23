cb035535eb1aaaab9f4aa6c78f426adf5df23870
pfjunior <pf.junior89@hotmail.com>
2023-06-23 15:57:52 -0300

    avancando com testes - aula 05

--- ddd-modelagem-patterns/src/entities/order.ts
+++ ddd-modelagem-patterns/src/entities/order.ts
@@ -13,19 +13,23 @@ export default class Order {
     }
 
     validate(): boolean {
-        if(this._id.length === 0) {
+        if (this._id.length === 0) {
             throw new Error("Id is required");
         }
-        if(this._customerId.length === 0) {
+        if (this._customerId.length === 0) {
             throw new Error("CustomerId is required");
         }
-        if(this._items.length === 0) {
-            throw new Error("Items quantity must be greater than 0");
+        if (this._items.length === 0) {
+            throw new Error("Items are required");
+        }
+
+        if (this._items.some(item => item.quantity <= 0)) {
+            throw new Error("Quantity must be greater than zero");
         }
         return true;
     }
 
     total(): number {
-        return this._items.reduce((acc, item) => acc + item._price, 0);
+        return this._items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
     }
 }
--- ddd-modelagem-patterns/src/tests/entities/order.spec.ts
+++ ddd-modelagem-patterns/src/tests/entities/order.spec.ts
@@ -17,21 +17,28 @@ describe("Order Unit Test", () => {
     it("should throw error when items is empty", () => {
         expect(() => {
             let order = new Order("o1", "c1", []);
-        }).toThrowError("Items quantity must be greater than 0");
+        }).toThrowError("Items are required");
     });
 
     it("should calculate total", () => {
-        const item1 = new OrderItem("i1", "Item 1", 100);
-        const item2 = new OrderItem("i1", "Item 2", 200);
+        const item1 = new OrderItem("i1", "Item 1", 100, "p1", 2);
+        const item2 = new OrderItem("i1", "Item 2", 200, "p2", 2);
         const order = new Order("o1", "c1", [item1]);
 
         let total = order.total();
 
-        expect(total).toBe(100);
+        expect(total).toBe(200);
 
         const order2 = new Order("o1", "c1", [item1, item2]);
         total = order2.total();
 
-        expect(total).toBe(300);
+        expect(total).toBe(600);
+    });
+
+    it("should throw error if the item quantity is less or equal than zero", () => {
+        expect(() => {
+            const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+            const order = new Order("o1", "c1", [item]);
+        }).toThrowError("Quantity must be greater than zero");
     });
 });
