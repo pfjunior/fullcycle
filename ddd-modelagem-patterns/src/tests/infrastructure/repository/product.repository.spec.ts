ac2c012a01e3795b36bed4f605687a4b9ecb9fb7
pfjunior <pf.junior89@hotmail.com>
2023-06-28 09:43:40 -0300

    repositories - aula 05

--- ddd-modelagem-patterns/src/tests/infrastructure/repository/product.repository.spec.ts
+++ ddd-modelagem-patterns/src/tests/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/db/sequelize/model/product.model";
 
 describe("Product Repository Test", () => {
     let sequileze: Sequelize;
@@ -10,6 +11,8 @@ describe("Product Repository Test", () => {
             logging: false,
             sync: { force: true },
         });
+        sequileze.addModels([ProductModel]);
+        await sequileze.sync();
     });
 
     afterEach(async () => {
