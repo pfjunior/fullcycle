30b0f4224b52fad9423dbd0615935a6f4e68afd8
pfjunior <pf.junior89@hotmail.com>
2023-07-04 15:45:25 -0300

    domain events - aula07

--- ddd-modelagem-patterns/src/tests/domain/events/event-dispatcher.spec.ts
+++ ddd-modelagem-patterns/src/tests/domain/events/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import SendEmailWhenProductIsCreatedHandler from "../../../domain/events/product/handler/send-email-when-product-is-created.handler";
+import ProductCreatedEvent from "../../../domain/events/product/product-created.event";
 import EventDispatcher from "../../../domain/events/shared/event-dispatcher";
 
 describe("Domain Events Tests", () => {
@@ -38,5 +39,21 @@ describe("Domain Events Tests", () => {
         eventDispatcher.unregisterAll();
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+    });
+
+    it("should notify all event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        const productCreatedEvent = new ProductCreatedEvent({ name: "Product 01", description: "Product 01 Descriptions", price: 10.0 });
+
+        eventDispatcher.notify(productCreatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
     });
 });
