d894eb8531d5b71594fa126df67ad2e021fcb57c
pfjunior <pf.junior89@hotmail.com>
2023-07-04 14:52:40 -0300

    domain events - aula03

---
+++ ddd-modelagem-patterns/src/tests/domain/events/event-dispatcher.spec.ts
@@ -0,0 +1,11 @@
+describe("Domain Events Tests", () => {
+    it("should register an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].lengt).toBe(1);
+    });
+});
