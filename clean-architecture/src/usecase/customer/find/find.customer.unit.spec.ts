e29f291bcb2bf4364d9275bc97708cd8ba0477c2
pfjunior <pf.junior89@hotmail.com>
2023-07-25 14:03:23 -0300

    iniciando com use cases - aula 06

--- clean-architecture/src/usecase/customer/find/find.customer.unit.spec.ts
+++ clean-architecture/src/usecase/customer/find/find.customer.unit.spec.ts
@@ -12,17 +12,13 @@ const MockRepository = () => {
         findAll: jest.fn(),
         create: jest.fn(),
         update: jest.fn()
-    }
+    };
 };
 
 describe("Unit Test Find Customer Use Case", () => {
     it("should find a customer", async () => {
         const customerRepository = MockRepository();
         const useCase = new FindCustomerUseCase(customerRepository);
-
-        const customer = new Customer("c1", "Customer 1");
-        customer.changeAddress(new Address("Street 1", 1, "12345-678", "City 1"));
-        await customerRepository.create(customer);
 
         const input = { id: "c1" };
 
@@ -40,5 +36,15 @@ describe("Unit Test Find Customer Use Case", () => {
         const result = await useCase.execute(input);
 
         expect(result).toEqual(output);
+    });
+
+    it("should not find a customer", async () => {
+        const customerRepository = MockRepository();
+        customerRepository.find.mockImplementation(() => { throw new Error("Customer not found") });
+        const useCase = new FindCustomerUseCase(customerRepository);
+
+        const input = { id: "c1" };
+
+        expect(() => { return useCase.execute(input) }).rejects.toThrow("Customer not found");
     });
 });
