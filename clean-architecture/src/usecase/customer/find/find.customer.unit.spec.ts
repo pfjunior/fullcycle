43907a4be58b831c9d815b7179119b81bb255cf3
pfjunior <pf.junior89@hotmail.com>
2023-07-25 13:50:17 -0300

    iniciando com use cases - aula 05

--- clean-architecture/src/usecase/customer/find/find.customer.unit.spec.ts
+++ clean-architecture/src/usecase/customer/find/find.customer.unit.spec.ts
@@ -1,31 +1,23 @@
 
-import { Sequelize } from "sequelize-typescript";
-import CustomerModel from "../../../infrastructure/customer/repositories/sequelize/customer.model";
-import CustomerRepository from "../../../infrastructure/customer/repositories/sequelize/customer.repository";
 import Customer from "../../../domain/customer/entities/customer";
 import Address from "../../../domain/customer/value-objects/address";
 import FindCustomerUseCase from "./find.customer.usecase";
 
-describe("Test Find Customer Use Case", () => {
-    let sequileze: Sequelize;
+const customer = new Customer("c1", "Customer 1");
+customer.changeAddress(new Address("Street 1", 1, "12345-678", "City 1"));
 
-    beforeEach(async () => {
-        sequileze = new Sequelize({
-            dialect: "sqlite",
-            storage: ":memory:",
-            logging: false,
-            sync: { force: true },
-        });
-        sequileze.addModels([CustomerModel]);
-        await sequileze.sync();
-    });
+const MockRepository = () => {
+    return {
+        find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn()
+    }
+};
 
-    afterEach(async () => {
-        await sequileze.close();
-    });
-
+describe("Unit Test Find Customer Use Case", () => {
     it("should find a customer", async () => {
-        const customerRepository = new CustomerRepository();
+        const customerRepository = MockRepository();
         const useCase = new FindCustomerUseCase(customerRepository);
 
         const customer = new Customer("c1", "Customer 1");
@@ -45,7 +37,7 @@ describe("Test Find Customer Use Case", () => {
             }
         };
 
-        const result = useCase.execute(input);
+        const result = await useCase.execute(input);
 
         expect(result).toEqual(output);
     });
