f0a4edb5fa90c59cf9951423329cc943c758db9e
pfjunior <pf.junior89@hotmail.com>
2023-08-01 09:49:55 -0300

    notification pattern - aula 02

---
+++ clean-architecture/src/domain/shared/notification/notification.ts
@@ -0,0 +1,22 @@
+export type NotificationError = {
+    message: string;
+    context: string;
+};
+
+export default class Notification {
+    private _errors: NotificationError[] = [];
+
+    addError(error: NotificationError) {
+        this._errors.push(error);
+    }
+
+    messages(context?: string): string {
+        let message = "";
+        this._errors.forEach((error) => {
+            if (context === undefined || error.context === context) {
+                message += `${error.context}: ${error.message},`;
+            }
+        });
+        return message;
+    }
+}
