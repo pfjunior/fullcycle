7440335b59af015e26bafab9006c5e504bc02df5
pfjunior <pf.junior89@hotmail.com>
2023-08-02 09:02:43 -0300

    validacoes-acoplamento - desafio

---
+++ clean-architecture/src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,32 @@
+import ValidatorInterface from "../../shared/validator/validator.interface";
+import * as yup from "yup";
+import Product from "../entities/product";
+
+export default class ProductYupValidator implements ValidatorInterface<Product>{
+    validate(entity: Product): void {
+        try {
+            yup.object()
+                .shape({
+                    id: yup.string().required("Id is required"),
+                    name: yup.string().required("Name is required"),
+                    price: yup.number().required().positive("Price must be greater than zero")
+                })
+                .validateSync({
+                    id: entity.id,
+                    name: entity.name,
+                    price: entity.price
+                }, {
+                    abortEarly: false
+                });
+        } catch (errors) {
+            const err = errors as yup.ValidationError;
+            err.errors.forEach((error) => {
+                entity.notification.addError({
+                    context: "product",
+                    message: error
+                });
+            });
+        }
+    }
+
+}
