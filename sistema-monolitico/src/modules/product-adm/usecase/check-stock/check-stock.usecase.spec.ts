9afc38f7d7b35f92e104093f44c1ea10fbc5b6c6
pfjunior <pf.junior89@hotmail.com>
2023-08-05 12:53:03 -0300

    store catalog - aula 01

---
+++ sistema-monolitico/src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,31 @@
+import Id from "../../../shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+
+const product = new Product({
+    id: new Id("p1"),
+    name: "Product 1",
+    description: "Product 1 Description",
+    purchasePrice: 100,
+    stock: 10
+});
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product))
+    };
+};
+
+describe("CheckStock UseCase Unit Test", () => {
+    it("should get stock of a product", async () => {
+        const repository = MockRepository();
+        const useCase = new CheckStockUseCase(repository);
+        const input = { productId: product.id };
+
+        const result = await useCase.execute(input);
+
+        expect(repository.find).toHaveBeenCalled();
+        expect(result.productId).toBe(product.id);
+        expect(result.stock).toBe(product.stock);
+    });
+});
