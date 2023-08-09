7938458a48a1ed9e36cab2c317a9b25f09bbd9a9
pfjunior <pf.junior89@hotmail.com>
2023-08-09 14:58:39 -0300

    modulo cliente adm - aula 06

--- sistema-monolitico/src/modules/client-adm/repository/client.repository.spec.ts
+++ sistema-monolitico/src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "./client.model";
 import ClientRepository from "./client.repository";
+import Client from "../domain/client.entity";
+import Id from "../../shared/domain/value-object/id.value-object";
 
 describe("Client Repository Test", () => {
     let sequelize: Sequelize;
@@ -35,6 +37,28 @@ describe("Client Repository Test", () => {
         const result = await repository.find(client.id);
 
         expect(result.id.id).toEqual(client.id);
+        expect(result.name).toEqual(client.name);
+        expect(result.email).toEqual(client.email);
+        expect(result.address).toEqual(client.address);
+        expect(result.createdAt).toEqual(client.createdAt);
+        expect(result.updatedAt).toEqual(client.updatedAt);
+    });
+
+    it("should create a client", async () => {
+        const client = new Client({
+            id: new Id("c1"),
+            name: "Client 1",
+            email: "client1@email.com",
+            address: "Address 1",
+        });
+
+        const repository = new ClientRepository();
+        await repository.add(client);
+
+        const result = await ClientModel.findOne({ where: { id: client.id.id } });
+
+        expect(result).toBeDefined();
+        expect(result.id).toEqual(client.id.id);
         expect(result.name).toEqual(client.name);
         expect(result.email).toEqual(client.email);
         expect(result.address).toEqual(client.address);
