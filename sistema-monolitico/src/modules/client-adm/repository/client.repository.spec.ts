093660cf19b6ddf362354e75d0353917d36e7bbf
pfjunior <pf.junior89@hotmail.com>
2023-08-09 14:32:20 -0300

    modulo cliente adm - aula 04

---
+++ sistema-monolitico/src/modules/client-adm/repository/client.repository.spec.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "./client.model";
+
+describe("Client Repository Test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true }
+        });
+
+        await sequelize.addModels([ClientModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+});
