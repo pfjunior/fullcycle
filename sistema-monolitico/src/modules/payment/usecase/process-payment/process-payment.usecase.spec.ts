8cda31aeb877116d19bf8bd4a1e263cee56256eb
pfjunior <pf.junior89@hotmail.com>
2023-08-10 09:04:57 -0300

    payment - aula 04

--- sistema-monolitico/src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
+++ sistema-monolitico/src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -14,6 +14,18 @@ const MockRepository = () => {
     };
 };
 
+const transactionDeclined = new Transaction({
+    id: new Id("t1"),
+    amount: 50,
+    orderId: "o1"
+});
+
+const MockRepositoryDeclined = () => {
+    return {
+        save: jest.fn().mockReturnValue(Promise.resolve(transactionDeclined))
+    };
+};
+
 describe("Process Payment UseCase Unit Test", () => {
     it("should approve a transaction", async () => {
         const repository = MockRepository();
@@ -31,5 +43,23 @@ describe("Process Payment UseCase Unit Test", () => {
         expect(result.orderId).toBe(transaction.orderId);
         expect(result.createdAt).toBe(transaction.createdAt);
         expect(result.updatedAt).toBe(transaction.updatedAt);
+    });
+
+    it("should decline a transaction", async () => {
+        const repository = MockRepositoryDeclined();
+        const useCase = new ProcessPaymentUseCase(repository);
+
+        const result = await useCase.execute({
+            orderId: transactionDeclined.orderId,
+            amount: transactionDeclined.amount
+        });
+
+        expect(repository.save).toHaveBeenCalled();
+        expect(result.transactionId).toBe(transactionDeclined.id.id);
+        expect(result.status).toBe("declined");
+        expect(result.amount).toBe(transactionDeclined.amount);
+        expect(result.orderId).toBe(transactionDeclined.orderId);
+        expect(result.createdAt).toBe(transactionDeclined.createdAt);
+        expect(result.updatedAt).toBe(transactionDeclined.updatedAt);
     });
 });
