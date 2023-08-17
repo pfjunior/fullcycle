9d2b74f6e6812cd9c816b0a6ad0dcd52a6b132dc
pfjunior <pf.junior89@hotmail.com>
2023-08-17 10:42:36 -0300

    remvendo o projeto sistema-monolitico

--- sistema-monolitico/src/modules/payment/Gateway/payment.gateway.ts
+++
@@ -1,5 +0,0 @@
-import Transaction from "../domain/transaction.entity";
-
-export default interface PaymentGateway {
-    save(input: Transaction): Promise<Transaction>;
-}
--- sistema-monolitico/src/modules/product-adm/facade/product-adm.facade.interface.ts
+++
@@ -1,6 +0,0 @@
-import { AddProductFacadeInputDto, CheckStockFacadeInputDto, CheckStockFacadeOutputDto } from "./product-adm.dto.facade.interace";
-
-export default interface ProducAdmFacadeInterface {
-    addProduct(input: AddProductFacadeInputDto): Promise<void>;
-    checkStock(input: CheckStockFacadeInputDto): Promise<CheckStockFacadeOutputDto>;
-}
