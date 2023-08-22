719fd7fe4cf45eb55d6bb90f4fdc8fbd393ca6ac
pfjunior <pf.junior89@hotmail.com>
2023-08-22 13:13:21 -0300

    checkout - aula 02

--- sistema-monolitico/src/modules/checkout/domain/client.entity.ts
+++ sistema-monolitico/src/modules/checkout/domain/client.entity.ts
@@ -1,35 +1,35 @@
-import AggregateRoot from '../../shared/domain/entities/aggregate-root.interface';
-import BaseEntity from '../../shared/domain/entities/base.entity';
-import Id from '../../shared/domain/value-object/id.value-object';
+import AggregateRoot from "../../shared/domain/entities/aggregate-root.interface";
+import BaseEntity from "../../shared/domain/entities/base.entity";
+import Id from "../../shared/domain/value-object/id.value-object";
 
 type ClientProps = {
-    id?: Id;
-    name: string;
-    email: string;
-    address: string;
+  id?: Id;
+  name: string;
+  email: string;
+  address: string;
 };
 
 export default class Client extends BaseEntity implements AggregateRoot {
-    private _name: string;
-    private _email: string;
-    private _address: string;
+  private _name: string;
+  private _email: string;
+  private _address: string;
 
-    constructor(props: ClientProps) {
-        super(props.id);
-        this._name = props.name;
-        this._email = props.email;
-        this._address = props.address;
-    }
+  constructor(props: ClientProps) {
+    super(props.id);
+    this._name = props.name;
+    this._email = props.email;
+    this._address = props.address;
+  }
 
-    get name():string{
-        return this._name;
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    get email():string{
-        return this._email;
-    }
+  get email(): string {
+    return this._email;
+  }
 
-    get address(): string{
-        return this._address;
-    }
+  get address(): string {
+    return this._address;
+  }
 }
