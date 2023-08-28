e4be240e918d2a79eaf7876eade4de49e03dc01a
pfjunior <pf.junior89@hotmail.com>
2023-08-28 08:53:38 -0300

    checkout - aula 07

--- sistema-monolitico/src/modules/checkout/usecase/place-order/placer-order.usecase.spec.ts
+++ sistema-monolitico/src/modules/checkout/usecase/place-order/placer-order.usecase.spec.ts
@@ -45,4 +45,64 @@ describe("Place Order UseCase Unit Test", () => {
       expect(mockValidateProducts).toHaveBeenCalledTimes(1);
     });
   });
+
+  describe("validateProducts methods", () => {
+    // @ts-expect-error - no params in constructor
+    const placerOrderUseCase = new PlaceOrderUseCase();
+
+    it("should throw error if no products are selected", async () => {
+      const input: PlaceOrderInputDto = {
+        clientId: "0",
+        products: [],
+      };
+
+      await expect(
+        placerOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("No products selected"));
+    });
+
+    it("should throw an error when product is out of stock", async () => {
+      const mockProductFacade = {
+        checkStock: jest.fn(({ productId }: { productId: string }) =>
+          Promise.resolve({ productId, stock: productId === "p1" ? 0 : 1 })
+        ),
+      };
+
+      // @ts-expect-error - force set productFacade
+      placerOrderUseCase["_productFacade"] = mockProductFacade;
+
+      let input: PlaceOrderInputDto = {
+        clientId: "0",
+        products: [{ productId: "p1" }],
+      };
+
+      await expect(
+        placerOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product p1 is not available in stock"));
+
+      input = {
+        clientId: "0",
+        products: [{ productId: "p1" }, { productId: "p2" }],
+      };
+
+      await expect(
+        placerOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product p1 is not available in stock"));
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3);
+
+      input = {
+        clientId: "0",
+        products: [
+          { productId: "p1" },
+          { productId: "p2" },
+          { productId: "p3" },
+        ],
+      };
+
+      await expect(
+        placerOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product p1 is not available in stock"));
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(6);
+    });
+  });
 });
